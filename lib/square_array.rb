9db09a3c3b47306896e891d58ee37a327d205111
JT Skidmore <johntoddskidmore@gmail.com>
2020-04-27 18:02:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array (array)
   new_array = []
   counter = 0
   while array[counter] do
-    new_array.push(array[counter]**)
+    new_array.push(array[counter]**2)
     counter +=
   end 
   new_array
